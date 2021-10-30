07b5c042b4e1da02cc8b2a045f51d19de9ab76a4
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2021-10-30 21:40:15 +0530

    created database models

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class Expense(models.Model):
+    name = models.CharField(max_length=50)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    categories = models.ManyToManyField(Category)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Expenses"
+        ordering = "-created_at"
