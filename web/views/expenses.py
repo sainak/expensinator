941748b16a06ce9a1e384f274680b55c4b1196a4
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2021-11-03 19:46:24 +0530

    added pagination to expenses

--- web/views/expenses.py
+++ web/views/expenses.py
@@ -1,13 +1,21 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from expenses.models import Expense
 
 
-class ExpenseListView(LoginRequiredMixin, TemplateView):
+class ExpenseListView(LoginRequiredMixin, ListView):
+
     login_url = reverse_lazy("login")
-    redirect_field_name = "next"
     template_name = "expenses/expense_list.html"
+    paginate_by = 15
+    context_object_name = "expenses_list"
     extra_context = {
         "title": "Expenses | Expensinator",
         "activeNavId": "navItemExpenses",
+        "currency": "₹"
     }
+
+    def get_queryset(self):
+        return Expense.objects.filter(owner=self.request.user)
