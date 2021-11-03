2cacc41428aeb6e6be5388d1f09861bcd15a5d19
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2021-11-03 16:18:38 +0530

    restructure templates

--- web/views/expenses.py
+++ web/views/expenses.py
@@ -6,4 +6,4 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class ExpenseListView(LoginRequiredMixin, TemplateView):
     login_url = reverse_lazy("login")
     redirect_field_name = "next"
-    template_name = "expense_list.html"
+    template_name = "expenses/expense_list.html"
