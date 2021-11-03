7fd25ed5bc6bfe49b3497871c1439b8f944100d5
Aakash Singh <46787056+sainAk@users.noreply.github.com>
2021-11-03 18:00:53 +0530

    dynamically update navbar elements

--- web/views/auth.py
+++ web/views/auth.py
@@ -9,12 +9,20 @@ class SignUpView(CreateView):
     form_class = SignUpForm
     success_url = reverse_lazy("login")
     template_name = "auth/signup.html"
+    extra_context = {
+        "title": "SignUp | Expensinator",
+        "activeNavId": "navItemSignUp",
+    }
 
 
 class LoginView(contrib_views.LoginView):
     form_class = LoginForm
     template_name = "auth/login.html"
     next_page = reverse_lazy("home")
+    extra_context = {
+        "title": "Login | Expensinator",
+        "activeNavId": "navItemLogin",
+    }
 
 
 class LogoutView(contrib_views.LogoutView):
--- web/views/main.py
+++ web/views/main.py
@@ -3,3 +3,8 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "home.html"
+    extra_context = {
+        "title": "Home | Expensinator",
+        "activeNavId": "navItemHome",
+    }
+
